211c4165ea0b236a9e86c088214110428c9b9478
jvmancuso <jvmancuso87@gmail.com>
2017-06-19 18:13:17 -0400

    adding dog-project

--- aind2-cnn/conv-dims.py
+++ aind2-cnn/conv-dims.py
@@ -2,6 +2,6 @@ from keras.models import Sequential
 from keras.layers import Conv2D
 
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=2, strides=2, padding='valid', 
-    activation='relu', input_shape=(200, 200, 1)))
+model.add(Conv2D(filters=32, kernel_size=3, strides=2, padding='same', 
+    activation='relu', input_shape=(128, 128, 3)))
 model.summary()
