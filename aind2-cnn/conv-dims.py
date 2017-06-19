744977fca9dbb01f90cd083e8e809c99e49e8531
jvmancuso <jvmancuso87@gmail.com>
2017-06-19 12:51:14 -0400

    improved IMDB SELU net

---
+++ aind2-cnn/conv-dims.py
@@ -0,0 +1,7 @@
+from keras.models import Sequential
+from keras.layers import Conv2D
+
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=2, strides=2, padding='valid', 
+    activation='relu', input_shape=(200, 200, 1)))
+model.summary()
